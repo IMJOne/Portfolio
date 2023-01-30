61ac77388f3b024654b00b09fc027c88dc3ac534
IMJOne <mompapa7@gmail.com>
2023-01-30 23:03:04 +0900

    Edit Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,51 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from '../styles/Sidebar.module.css';
 
-import { ReactComponent as File } from '../icons/file.svg';
-import { ReactComponent as Code } from '../icons/code.svg';
-import { ReactComponent as Mail } from '../icons/mail.svg';
-import { ReactComponent as Pencil } from '../icons/pencil.svg';
-import { ReactComponent as Extension } from '../icons/extension.svg';
-import { ReactComponent as User } from '../icons/user.svg';
-import { ReactComponent as Setting } from '../icons/setting.svg';
-
-import { Link } from 'react-router-dom';
+import FileIcon from '../icons/FileIcon';
+import CodeIcon from '../icons/CodeIcon';
+import MailIcon from '../icons/MailIcon';
+import PencilIcon from '../icons/PencilIcon';
+import ExtensionIcon from '../icons/ExtensionIcon';
+import UserIcon from '../icons/UserIcon';
+import SettingIcon from '../icons/SettingIcon';
 
 const topMenu = [
-  { path: '/', icon: <File />, text: 'home' },
-  { path: '/about', icon: <Code />, text: 'about' },
-  { path: '/skills', icon: <Mail />, text: 'skills' },
-  { path: '/study', icon: <Pencil />, text: 'study' },
-  { path: '/projects', icon: <Extension />, text: 'projects' },
+  { Icon: FileIcon, path: '/', text: 'home' },
+  { Icon: CodeIcon, path: '/about', text: 'about' },
+  { Icon: MailIcon, path: '/skills', text: 'skills' },
+  { Icon: PencilIcon, path: '/study', text: 'study' },
+  { Icon: ExtensionIcon, path: '/projects', text: 'projects' },
 ];
 const bottomMenu = [
-  { path: '/readme', icon: <User />, text: 'readme' },
-  { path: '/setting', icon: <Setting />, text: 'setting' },
+  { Icon: UserIcon, path: '/readme', text: 'readme' },
+  { Icon: SettingIcon, path: '/setting', text: 'setting' },
 ];
 
 export default function Sidebar() {
+  const createMenu = menu => {
+    return menu.map(({ Icon, path, text }, index) => (
+      <Link key={index} className={styles.menu} to={path}>
+        <Icon className={styles.icon} />
+        <div className={styles.tooltip}>{text}</div>
+      </Link>
+    ));
+  };
+
   return (
     <aside className={styles.sidebar}>
-      <div>
-        {topMenu.map((item, index) => (
-          <Link key={index} className={styles.menu} to={item.path}>
-            <button className={styles.icon}>{item.icon}</button>
-            <Tooltip text={item.text} />
-          </Link>
-        ))}
-      </div>
-      <div>
-        {bottomMenu.map((item, index) => (
-          <Link key={index} className={styles.menu} to={item.path}>
-            <button className={styles.icon}>{item.icon}</button>
-            <Tooltip text={item.text} />
-          </Link>
-        ))}
-      </div>
+      <div>{createMenu(topMenu)}</div>
+      <div>{createMenu(bottomMenu)}</div>
     </aside>
   );
-}
-
-function Tooltip({ text }) {
-  return <div className={styles.tooltip}>{text}</div>;
 }
