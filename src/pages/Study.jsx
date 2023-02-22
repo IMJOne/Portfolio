9f96776e4765a34b9b1ce4a904ee122428f8efeb
IMJOne <mompapa7@gmail.com>
2023-02-22 19:01:53 +0900

    Change notion page

--- src/pages/Study.jsx
+++ src/pages/Study.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from '../styles/Study.module.css';
 
 import Note from '../components/Note';
-import { notion } from '../util/notion';
+import { notionList } from '../util/notionList';
 
 export default function Study() {
   return (
     <div>
       <h2>Study Record 📚</h2>
       <div className={styles.container}>
-        {notion.map(({ image, title, url }, index) => (
+        {notionList.map(({ image, title, url }, index) => (
           <Note key={index} image={image} title={title} url={url} />
         ))}
       </div>
--- src/util/notionList.js
+++ src/util/notionList.js
@@ -1,18 +1,18 @@
-export const notion = [
+export const notionList = [
   {
     image: 'images/page_react.png',
     title: '[React] 컴포넌트 기반의 자바스크립트 UI 라이브러리, 리액트 총정리',
     url: 'https://imjone.notion.site/React-c516a764ebee4d8e898d714a85d9475b',
   },
   {
-    image: 'images/page_js.png',
-    title: '[JavaScript] 자바스크립트 ES6+ 문법 및 내부 구현 사항 총정리',
-    url: 'https://imjone.notion.site/JavaScript-5fc08fb5d6bd4bef838e275d4d924f4b',
+    image: 'images/page_ts.jpg',
+    title: '[TS] 타입스크립트의 강력한 타입 시스템과 객체 지향 프로그래밍',
+    url: 'https://imjone.notion.site/TypeScript-7176d1d3d64f467bb6c819b5e9ad5e72',
   },
   {
-    image: 'images/page_css.png',
-    title: '[CSS] CSS 사용법과 예제 총정리 (문법, 반응형)',
-    url: 'https://www.notion.so/imjone/CSS-dab3f2d547cc482da98b8f5cdc1434ac',
+    image: 'images/page_js.png',
+    title: '[JS] 자바스크립트 ES6+ 기본 문법 및 내부 구현 사항 총정리',
+    url: 'https://imjone.notion.site/JavaScript-5fc08fb5d6bd4bef838e275d4d924f4b',
   },
   {
     image: 'images/page_git.png',
@@ -20,13 +20,13 @@ export const notion = [
     url: 'https://imjone.notion.site/Git-fdf84d8e4e224853bac94b8886d8a373',
   },
   {
-    image: 'images/page_error.png',
-    title: '[Error Note] 에러 노트 정리',
-    url: 'https://imjone.notion.site/Error-Note-7d12a9a9e92c47be8c31318ebe58a01f',
-  },
-  {
     image: 'images/page_project.png',
     title: '[Project] 프로젝트 소스 코드 및 회고록',
     url: 'https://imjone.notion.site/3fe849b68fa948219b7265fa0fcd479c?v=fe57bbe9842f44909bd6a5afb68ae2d4',
+  },
+  {
+    image: 'images/page_error.png',
+    title: '[Error Note] 에러 노트 정리',
+    url: 'https://imjone.notion.site/Error-Note-7d12a9a9e92c47be8c31318ebe58a01f',
   },
 ];
